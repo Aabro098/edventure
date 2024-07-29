b33da86a5723ea2f8eba45e896490c1352a3a328
Aabro098 <arbinstha71@gmail.com>
2024-07-29 20:54:01 +0545

    connected the database

--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
-
-
 const express = require("express");
 const mongoose = require("mongoose");
 const http = require("http");
+
+const DB = "mongodb+srv://arbinstha71:Aabro098@cluster0.m51ocmp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const authRouter = require("./routes/auth");
 
 const PORT = 3000;
 
 const app = express();
 
-app.listen(PORT, "" , () => {
-    console.log(`Connnected to the port ${PORT}`);
-})
+app.use(authRouter);
+
+mongoose
+    .connect(DB)
+    .then(() => {
+        console.log("Connection Successful");
+    }).catch((e) => {
+        console.log(e);
+    })
+
+app.listen(PORT, () => {
+    console.log(`Connected to the port ${PORT}`);
+});
---
+++ server/routes/auth.js
@@ -0,0 +1,10 @@
+
+const express = require("express");
+
+const authRouter = express.Router();
+
+authRouter.post("/api/signup" , (req,res) =>{
+    
+})
+
+module.exports = authRouter;
