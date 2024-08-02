3b84b07101bb47a771d80b1efadaf58c5f1f25b6
Aabro098 <arbinstha71@gmail.com>
2024-08-02 14:29:37 +0545

    creating the profile screen

---
+++ lib/Widgets/stars.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class Star extends StatelessWidget {
+  const Star({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Icon(Icons.star, color: Colors.yellow, size: 30,);
+  }
+}
