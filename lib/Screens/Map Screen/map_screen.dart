2002e268c0058838b0c6982165396fd3e94f8323
Aabro098 <arbinstha71@gmail.com>
2024-08-30 10:56:09 +0545

    added map to the app

--- lib/Screens/Map Screen/map_screen.dart
+++ lib/Screens/Map Screen/map_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -8,10 +10,62 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  static const LatLng _pKathmandu = LatLng(27.7172, 85.3240);
+  late GoogleMapController _controller;
+  LatLng? _currentLocation;
+  final Location _locationService = Location();
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocationPermission();
+  }
+
+  Future<void> _getLocationPermission() async {
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+
+    serviceEnabled = await _locationService.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await _locationService.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await _locationService.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await _locationService.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    final locationData = await _locationService.getLocation();
+    setState(() {
+      _currentLocation = LatLng(locationData.latitude!, locationData.longitude!);
+    });
+
+    _controller.animateCamera(CameraUpdate.newLatLng(_currentLocation!));
+    }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Scaffold()
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _currentLocation ?? _pKathmandu,
+          zoom: 13,
+        ),
+        onMapCreated: (GoogleMapController controller) {
+          _controller = controller;
+        },
+        mapType: MapType.normal,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        zoomGesturesEnabled: true,
+        scrollGesturesEnabled: true,
+      ),
     );
   }
 }
