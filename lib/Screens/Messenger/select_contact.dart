d0a2fec7958c54bea004fd80d62250c218c8e132
Aabro098 <arbinstha71@gmail.com>
2024-10-23 13:25:03 +0545

    addding the file picker to the dependencies

--- lib/Screens/Messenger/select_contact.dart
+++ lib/Screens/Messenger/select_contact.dart
@@ -43,120 +43,122 @@ class _SelectContactState extends State<SelectContact> {
   
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue.shade100,
-        elevation: 0.0,
-        leadingWidth: 250,
-        leading: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Text(
-            'Select Contact',
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.bold
-            ),
-          )
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon : Icon(
-              Icons.arrow_back
-            ),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: _searchController,
-                decoration: InputDecoration(
-                  hintText: 'Search users...',
-                  hintStyle: TextStyle(
-                    color: Colors.grey
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                    borderSide: const BorderSide(
-                      color: Colors.lightBlue,
-                      width: 2.0
-                    )
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                    borderSide: const BorderSide(
-                      color: Colors.blue, 
-                      width: 2.0,        
-                    ),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12.0),
-                    borderSide: const BorderSide(
-                      color: Colors.cyan,
-                      width: 1.0,        
-                    ),
-                  ),
-                  suffixIcon: _isSearching
-                      ? Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: CircularProgressIndicator(strokeWidth: 2),
-                        )
-                      : IconButton(
-                          icon: const Icon(Icons.search),
-                          onPressed: () {
-                            _searchUsers(_searchController.text);
-                          },
-                        ),
-                ),
-                onChanged: (query) {
-                  _searchUsers(query); 
-                },
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade100,
+          elevation: 0.0,
+          leadingWidth: 250,
+          leading: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              'Select Contact',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold
               ),
-              const SizedBox(height: 16.0),
-              
-              Expanded(
-                child: _searchResults.isEmpty
-                ? Center(
-                    child: _isSearching
-                        ? CircularProgressIndicator()
-                        : const Text(
-                            'No results found',
-                            style: TextStyle(color: Colors.grey),
+            )
+          ),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon : Icon(
+                Icons.arrow_back
+              ),
+            ),
+          ],
+        ),
+        body: Column(
+          children: [
+            Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    hintText: 'Search users...',
+                    hintStyle: TextStyle(
+                      color: Colors.grey
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(
+                        color: Colors.lightBlue,
+                        width: 2.0
+                      )
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(
+                        color: Colors.blue, 
+                        width: 2.0,        
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                      borderSide: const BorderSide(
+                        color: Colors.cyan,
+                        width: 1.0,        
+                      ),
+                    ),
+                    suffixIcon: _isSearching
+                        ? Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: CircularProgressIndicator(strokeWidth: 2),
+                          )
+                        : IconButton(
+                            icon: const Icon(Icons.search),
+                            onPressed: () {
+                              _searchUsers(_searchController.text);
+                            },
                           ),
-                  )
-                : ListView.builder(
-                    itemCount: _searchResults.length,
-                    itemBuilder: (context, index) {
-                      final user = _searchResults[index];
-                      return Padding(
-                        padding: const EdgeInsets.only(bottom: 8.0),
-                        child: FriendCard(
-                          suggested: false,
-                          user: user,
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => IndividualChat(),
-                              ),
-                            );
-                          },
-                        ),
-                      );
-                    },
                   ),
-              ),
-            ],
+                  onChanged: (query) {
+                    _searchUsers(query); 
+                  },
+                ),
+                const SizedBox(height: 16.0),
+                
+                Expanded(
+                  child: _searchResults.isEmpty
+                  ? Center(
+                      child: _isSearching
+                          ? CircularProgressIndicator()
+                          : const Text(
+                              'No results found',
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                    )
+                  : ListView.builder(
+                      itemCount: _searchResults.length,
+                      itemBuilder: (context, index) {
+                        final user = _searchResults[index];
+                        return Padding(
+                          padding: const EdgeInsets.only(bottom: 8.0),
+                          child: FriendCard(
+                            suggested: false,
+                            user: user,
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => IndividualChat(),
+                                ),
+                              );
+                            },
+                          ),
+                        );
+                      },
+                    ),
+                ),
+              ],
+            ),
           ),
+          ],
         ),
-        ],
       ),
     );
   }
